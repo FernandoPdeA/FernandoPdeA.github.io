f35dd655444a0c46674d8c5ca604f8616f197197
Fernando <pupygreen@gmail.com>
2022-07-15 13:26:54 -0300

    Criação de modal de resposta do formulário

---
+++ index.js
@@ -0,0 +1,24 @@
+//Abria a modal usando javascript logo após o cadastro de usuário
+
+window.onload = function () {
+
+  var modal = document.querySelector('#modal');
+  var btn = document.querySelector("#btn");
+  var closed = document.querySelector("#close");
+  var email = document.querySelector("input[type=email]");
+
+  btn.onclick = function (e) {
+    e.preventDefault();
+    if (email.value != "" && email.value != null) {
+      modal.style.display = "block";
+    } else {
+      alert("Preencha os campos corretamente!");
+    }
+
+    closed.onclick = function (e) {
+      e.preventDefault();
+      modal.style.display = "none";
+      window.location.reload();
+    }
+  }
+}
