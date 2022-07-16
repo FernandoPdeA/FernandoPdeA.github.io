16e81be1c47b7ce6f908864a448d8d8bb358c293
Fernando <pupygreen@gmail.com>
2022-07-16 17:09:38 -0300

    Reajustes em formuláro de contato

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ window.onload = function () {
   var modal = document.querySelector('#modal');
   var btn = document.querySelector("#btn");
   var closed = document.querySelector("#close");
-  var email = document.querySelector("input[type=email]");
+  var email = document.querySelector("#email");
 
   btn.onclick = function (e) {
     e.preventDefault();
     if (email.value != "" && email.value != null) {
       modal.style.display = "block";
     } else {
-      alert("Preencha os campos corretamente!");
+      alert("Preencha todos os campos corretamente!");
     }
 
     closed.onclick = function (e) {
